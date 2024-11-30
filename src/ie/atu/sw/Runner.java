c079a73fcf994bfb91417087c71a9e2b6ad169ef
Conor Dowdall <conordowdallmail@gmail.com>
2024-11-30 10:05:44 +0000

    abstract the Scanner to an InputReader

--- src/ie/atu/sw/Runner.java
+++ src/ie/atu/sw/Runner.java
@@ -2,9 +2,11 @@ package ie.atu.sw;
 
 import java.util.Scanner;
 
+import ie.atu.sw.console.ConsoleInputReader;
 import ie.atu.sw.menu.ConsoleMenuPrinter;
 import ie.atu.sw.menu.MenuPrinter;
 import ie.atu.sw.simplifiermenu.SimplifierMainMenu;
+import ie.atu.sw.util.InputReader;
 import ie.atu.sw.wordreplacer.WordReplacer;
 
 public class Runner {
@@ -12,10 +14,14 @@ public class Runner {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+
         MenuPrinter menuPrinter = new ConsoleMenuPrinter();
+
+        InputReader inputReader = new ConsoleInputReader(scanner, menuPrinter);
+
         WordReplacer wordReplacer = new WordReplacer();
 
-        new SimplifierMainMenu(scanner, menuPrinter, wordReplacer)
+        new SimplifierMainMenu(inputReader, menuPrinter, wordReplacer)
                 .printMenuAndAcceptChoice();
 
     }
